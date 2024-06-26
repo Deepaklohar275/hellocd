2e966febeb5669d5f91d2afc6494a103571ec797
Deepak lohar <Deepaklohar275@gmail.com>
2024-06-26 16:01:24 +0530

    changing the print messgae

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello Continuous delivery";
 	}
 
 	public static void main(String[] args) {
